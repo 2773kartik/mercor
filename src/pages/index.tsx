083fd7634c0d8b849b6331e9d6b0e04c5294aa01
Kartik Tiwari <mekartik27@gmail.com>
2023-07-02 01:51:59 +0530

    Posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import Head from "next/head";
-import Link from "next/link";
-import { api } from "~/utils/api";
 import Navbar from "~/components/navbar";
-import Landing from "../components/landing";
+import Landing from "~/components/landing";
 import Footer from "~/components/footer";
+import { useUser } from "@clerk/nextjs";
+
 
 export default function Home() {
-  const {data, isLoading} = api.posts.getAll.useQuery();
+  const user = useUser();
   return (
     <>
       <Head>
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,8 +1,38 @@
+import { clerkClient } from "@clerk/nextjs";
 import { z } from "zod";
+import type { User } from "@clerk/nextjs/api";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { fileURLToPath } from "url";
+import { use } from "react";
+import { TRPCError } from "@trpc/server";
+
+const filterUser = (user: User) => {
+  return {id: user.id, name:user.username, profilePic:user.profileImageUrl}
+}
 
 export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+    });
+
+    const users = (
+      await clerkClient.users.getUserList({
+      userId: posts.map((post) => post.authorId),
+      limit: 100,
+    })
+    ).map(filterUser);
+
+    return posts.map((post) => {
+      const author = users.find((user) => user.id === post.authorId);
+      if(!author) 
+        throw new TRPCError({ 
+          code: "INTERNAL_SERVER_ERROR",
+          message:"Author for post not found",
+        });
+      return {
+      post,
+      author,
+   }})
   }),
 });
