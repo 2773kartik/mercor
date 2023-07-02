2275ed3413c7aca03e8eb566a47f60c01d91197f
Yashasv-Prajapati <yamuprajapati05@gmail.com>
2023-07-03 01:02:43 +0530

    minor changes

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -16,10 +16,10 @@ export default function Navbar() {
     return (
       <nav className="sticky top-0 bg-white border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-2">
-          <a href="/" className="flex items-center">
+          <Link href="/" className="flex items-center">
             <Image src={Logo} alt="SkillShow Logo" className="h-8 w-8 mr-3" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">SkillShow</span>
-          </a>
+          </Link>
           <button
             type="button"
             className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
