9dfe58f329645f44491d122ce72ae9113ae689ca
Kartik Tiwari <mekartik27@gmail.com>
2023-07-02 20:00:44 +0530

    Add Skill to User

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -3,6 +3,7 @@ import { api } from "~/utils/api"
 import { useUser } from "@clerk/nextjs"
 
 import { ChakraBaseProvider, extendBaseTheme } from '@chakra-ui/react'
+import { toast } from "react-toastify";
 
 
 import {
@@ -30,8 +31,34 @@ export default function Dashboard(){
     const myuser = useUser();
 
     const details = api.profile.getData.useQuery();
+    console.log(details.data);
 
     const totalLikes = api.profile.getTotalLikes.useQuery();
+
+    const addSkill = api.users.addskill.useMutation();
+    const {data} = api.skill.getApprovedSkills.useQuery();
+
+    const [selectedOption, setSelectedOption] = useState<string>('');
+
+    function handleSkillChange(e: ChangeEvent<HTMLSelectElement>) {
+        setSelectedOption(e.target.value);   
+    }
+
+    async function handleSkillAdd(e:FormEvent) {
+      e.preventDefault();
+      if(selectedOption==='' || selectedOption===null || selectedOption===undefined) {
+        toast.warning("Select a tag please!");
+        return;
+      }
+      if (!details.data?.id) {
+        console.log("User is not defined");
+        return;
+      }
+      addSkill.mutate({
+        id: details.data?.id,
+        skillTag: [selectedOption],
+      })
+    }
 
     return (
         <ChakraBaseProvider theme={theme}>
@@ -64,7 +91,31 @@ export default function Dashboard(){
                 <StatNumber>{details.data?.rating}</StatNumber>
               </Stat>
 
+              <Stat>
+                <StatLabel>Rating</StatLabel>
+                <StatNumber>{details.data?.rating}</StatNumber>
+              </Stat>
+
             </Flex>
+
+            <form onSubmit={handleSkillAdd}>
+            <div className="w-4/5 mx-auto  flex flex-col">
+                
+                <select
+                    className="p-2 m-2 text-black border-2 shadow-md rounded outline-none cursor-pointer"
+                    value={selectedOption}
+                    onChange={handleSkillChange}
+                    >
+                    {data?.map((skill: { id: string; name: string, approved: boolean }) => (
+                        <option key={skill.id} value={skill.id}>
+                        {skill.name}
+                        </option>
+                    ))}
+                </select>
+                <button className="shadow-md bg-orange-400 w-1/2 mx-auto rounded m-2 p-2">Publish</button>
+
+            </div>
+        </form>
 
           </Box>
         </ChakraBaseProvider>
