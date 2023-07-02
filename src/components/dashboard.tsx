81e259223f35491756b787ad0c79d6ecc2256760
Arker123 <kharbandarnav@gmail.com>
2023-07-03 00:47:51 +0530

    Refractored profile design

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -76,61 +76,50 @@ export default function Dashboard(){
     }
 
     return (
-        <ChakraBaseProvider theme={theme}>
-          <Box p={8}>
-            <Flex alignItems="center" mb={6}>
-                <Box p={10}>
-                    <Avatar src={myuser.user?.profileImageUrl} size="xs" mr={20} />
+      <ChakraBaseProvider theme={theme}>
+      <Box position='relative' h='45rem' w='100%' bgGradient="linear(to-r, #766DFF, #88F3FF)">
+        <AbsoluteCenter axis='both' p={8} w='50%' bg={'gray.200'} borderRadius='lg'>
+            <Grid templateColumns='repeat(2, 1fr)' gap={0}>
+              <GridItem w='100%' h='20%'>
+                <Box p={4} textAlign='center' >
+                    <Avatar src={myuser.user?.profileImageUrl} size="xs" m={10} />
                 </Box>
-              <Box>
-                <Heading>{myuser.user?.fullName}</Heading>
-                <Box>{myuser.user?.emailAddresses[0]?.emailAddress}</Box>
-              </Box>
-            </Flex>
-            <Flex alignItems="center" mb={6}>
+              </GridItem>
+              <GridItem w='100%' h='20%' verticalAlign='center'>
+                <Box alignContent={'center'} m={'10'}>
+                  <Heading fontSize={30} fontWeight={'bold'} fontFamily='"Heebo", sans-serif'>{myuser.user?.fullName}</Heading>
+                  <Box fontSize={13} fontWeight={'bold'} mt={3} fontFamily="'Roboto', sans-serif">{myuser.user?.emailAddresses[0]?.emailAddress}</Box>
 
-            </Flex>
-            <Flex justifyContent="space-between" mb={8}>
-              <Stat>
-                <StatLabel>Total Likes</StatLabel>
-                <StatNumber>{totalLikes.data}</StatNumber>
-              </Stat>
+                  <Box fontFamily={'Roboto'} mt={4}> Total Likes: {totalLikes.data}</Box>
+                  <Box fontFamily={'Roboto'} mt={4}> Karma Points: {details.data?.karma}</Box>
+                  <Box fontFamily={'Roboto'} mt={4}> Rating: {details.data?.rating}</Box>
+                  <Box fontFamily={'Roboto'} mt={4} alignItems={'center'} textAlign={'center'} > Add Skill</Box>
 
-              <Stat>
-                <StatLabel>Karma Points</StatLabel>
-                <StatNumber>{details.data?.karma}</StatNumber>
-              </Stat>
+                  <form onSubmit={handleSkillAdd}>
+                    <div className="w-4/5 mx-auto flex flex-col">
+                        
+                        <select
+                            className="p-2 m-2 text-black border-2 shadow-md rounded outline-none cursor-pointer"
+                            value={selectedOption}
+                            onChange={handleSkillChange}
+                            >
+                            {data?.map((skill: { id: string; name: string, approved: boolean }) => (
+                                <option key={skill.id} value={skill.id}>
+                                {skill.name}
+                                </option>
+                            ))}
+                        </select>
+                        <button className="shadow-md bg-orange-400 w-1/2 mx-auto rounded m-2 p-2">Publish</button>
 
-              <Stat>
-                <StatLabel>Rating</StatLabel>
-                <StatNumber>{details.data?.rating}</StatNumber>
-              </Stat>
+                    </div>
+                </form>
 
-              <Stat>
-                <StatLabel>Rating</StatLabel>
-                <StatNumber>{details.data?.rating}</StatNumber>
-              </Stat>
+                </Box>
+              </GridItem>
+            </Grid>
+        </AbsoluteCenter>
 
-            </Flex>
-
-            <form onSubmit={handleSkillAdd}>
-            <div className="w-4/5 mx-auto flex flex-col">
-                
-                <select
-                    className="p-2 m-2 text-black border-2 shadow-md rounded outline-none cursor-pointer"
-                    value={selectedOption}
-                    onChange={handleSkillChange}
-                    >
-                    {data?.map((skill: { id: string; name: string, approved: boolean }) => (
-                        <option key={skill.id} value={skill.id}>
-                        {skill.name}
-                        </option>
-                    ))}
-                </select>
-                <button className="shadow-md bg-orange-400 w-1/2 mx-auto rounded m-2 p-2">Publish</button>
-
-            </div>
-        </form>
+            
 
           </Box>
         </ChakraBaseProvider>
