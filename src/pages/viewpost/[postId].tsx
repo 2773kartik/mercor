0066a27530dbcd23a512ac2201acd7fd7aaceec5
Arker123 <kharbandarnav@gmail.com>
2023-07-02 23:26:53 +0530

    [Feature] Added comments

--- src/pages/viewpost/[postId].tsx
+++ src/pages/viewpost/[postId].tsx
@@ -56,6 +56,12 @@ export default function ViewPost(){
     }
 };
 
+    const newComment = (postId: any) => {
+        console.log(postId);
+        router.push(`/newcomment/${postId}`);
+    }
+
+
 
 
 
@@ -85,9 +91,11 @@ export default function ViewPost(){
                     <span>{currState}</span>
                 </div>
                 <div className="flex text-gray-700 text-sm mr-8">
-                    <svg fill="none" viewBox="0 0 24 24" className="w-4 h-4 mr-1" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z" />
-                    </svg>
+                    <div onClick={ () => newComment(postId) }>
+                        <svg fill="none" viewBox="0 0 24 24" className="w-4 h-4 mr-1" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z" />
+                        </svg>
+                    </div>
                     
                 </div>
                 <div className="flex text-gray-700 text-sm mr-4">
